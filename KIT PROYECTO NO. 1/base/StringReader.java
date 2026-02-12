57baba36ea9a67aaaf651484ffdd40076c21f735
Josue Villeda <josuevilleda@MacBook-Air-de-Josue.local>
2026-02-11 23:02:35 -0600

    datos de la clase

---
+++ KIT PROYECTO NO. 1/base/StringReader.java
@@ -0,0 +1,15 @@
+public class StringReader {
+    public StringReader(String filePath) {
+        // Constructor de la clase. Aqui debe leer el archivo y almacenar todas las cadenas que contiene
+    }
+
+    public int length() {
+        // modifique este metodo para que devuelva la cantidad correcta de cadenas a evaluar
+        return 0;
+    }
+
+    public String get(int index) {
+        // modifique este metodo para que devuelva la cadena en la posicion index
+        return "";
+    }
+}
---
+++ KIT PROYECTO NO. 1/base/StringWriter.java
@@ -0,0 +1,13 @@
+public class StringWriter {
+    public StringWriter(String filePath) {
+        // Constructor de la clase. Aqui debe crear un FileWriter o una estructura similar para escribir a disco
+    }
+
+    public void write(boolean result) {
+        // Escribe "true" o "false" en el archivo, uno por linea
+    }
+
+    public void close() {
+        // Cierra el archivo
+    }
+}
