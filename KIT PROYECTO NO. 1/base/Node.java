dc5145d779636a3bc36e3db693471a01a72274a9
MonteNegroA <25000101@galileo.edu>
2026-02-14 22:35:52 -0600

    Add Node class for tree structure implementation
    Clase del objeto de nodos

---
+++ KIT PROYECTO NO. 1/base/Node.java
@@ -0,0 +1,32 @@
+public class Node<N>{
+    //Cada nodo almacena la informacion necesaria de:
+    /* padre
+    * nodo hijo izquierda
+    * nodo hijo derecha
+    * hz indica si posee hijo izquierdo. 
+    * hd indica si posee hijo derecha
+    * NOTAR: que no son valores fijos, sino cambiantes y solo sirven
+    * para almacenar si se posee hijo izquierda o derecha, si no tiene, o ya fue ocupado.*/
+    Node <N> parent, left, right;
+    char dato;
+    char hz;
+    char hd;
+
+    Node(Node <N> parent, char dato, Node <N> left, Node <N> right, char hz, char hd){
+        this.parent = parent;
+        this.dato = dato;
+        this.left = left;
+        this.right = right;
+        this.hz = hz;
+        this.hd = hd;
+    }
+
+    public void getChangeI(char s){
+        this.hz = s;
+    }
+    public void getChangeD(char s){
+        this.hd = s;
+    }
+
+
+}
